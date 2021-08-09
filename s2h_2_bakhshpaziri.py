a8bb0b3c7f827a11feac8f356844cd8f7c1c808c
Eskaandari <87777504+Eskaandari@users.noreply.github.com>
2021-08-09 14:59:10 +0430

    Create s2h_2_bakhshpaziri.py

---
+++ s2h_2_bakhshpaziri.py
@@ -0,0 +1,8 @@
+#barnamei hastesh baraye inke check konad ke
+#adade aval bar adade dovom bakhsh pazir ast ya kheyr
+first_number=int(input("adad aval ra vared konid:"))
+second_number=int(input("adade dovom ra vared konid:"))
+if first_number % second_number == 0:
+    print("adade aval bar adade dovom bakhsh pazir ast")
+else:
+    print("adade aval bar adade dovom bakhsh pazir nist")
