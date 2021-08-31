e6001e07f89724f8b0444d71771865a00b297744
Eskaandari <87777504+Eskaandari@users.noreply.github.com>
2021-09-01 03:57:10 +0430

    Create s4h_2_dictionary.py

---
+++ s4h_2_dictionary.py
@@ -0,0 +1,8 @@
+#barnameyi baraye gereftane moshakhasat az fard va chap kardane anha dar dictionary
+name = input("nam:\n")
+last_name = input("name khanevadegi:\n")
+age = int(input("sen:\n"))
+vaziate_taahol = input("vaziate_taahol:\n")
+human = {"name":name, "last_name":last_name, "age":age, "vaziate_taahol":vaziate_taahol}
+print(human)
+print(type(human))
